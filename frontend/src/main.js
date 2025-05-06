bfc54425fe2c83c71dd96e4f6aa8e5f03841925a
YanYamim <yanyamim@gmail.com>
2025-05-06 11:32:34 -0300

    instalando vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,23 @@
-import { createApp } from 'vue'
+/**
+ * main.js
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+
+// Components
 import App from './App.vue'
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+// Composables
+import { createApp } from 'vue'
 
-const vuetify = createVuetify({
-  components,
-  directives,
-})
+// Styles
+import 'unfonts.css'
 
-createApp(App).use(vuetify).mount('#app')
+const app = createApp(App)
+
+registerPlugins(app)
+
+app.mount('#app')
