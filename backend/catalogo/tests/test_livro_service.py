32acf2b7e228603c1c938c9b2c0369b38f450fb7
YanYamim <yanyamim@gmail.com>
2025-05-19 20:22:17 -0300

    terminando testes unitários

--- backend/catalogo/tests/test_livro_service.py
+++ backend/catalogo/tests/test_livro_service.py
@@ -7,7 +7,7 @@ from rest_framework import status
 def test_listar_livros_sucesso():
     dados = {
         "nome_livro": "1984",
-        "sinopse": "Várias fita"
+        "sinopse_livro": "Várias fita"
     }
 
     cadastrar_livros(dados)
@@ -16,15 +16,37 @@ def test_listar_livros_sucesso():
     assert isinstance(resultado, list)
     assert len(resultado) == 1
     assert resultado[0]['nome_livro'] == "1984"
+    assert resultado[0]['sinopse_livro'] == "Várias fita"
 
 @pytest.mark.django_db
 def test_cadastrar_livros_sucesso():
     dados = {
         "nome_livro": "1984",
-        "sinopse": "Várias fita"
+        "sinopse_livro": "Várias fita"
     }
     resultado = cadastrar_livros(dados)
     assert resultado['status'] == status.HTTP_201_CREATED
     assert resultado['data']['nome_livro'] == "1984"
+    assert resultado['data']['sinopse_livro'] == "Várias fita"
     assert Livro.objects.count() == 1
 
+@pytest.mark.django_db
+def test_editar_livros_sucesso():
+    dados = {
+        "nome_livro": "1984",
+        "sinopse_livro": "Várias fita"
+    }
+
+    resultado_cadastro = cadastrar_livros(dados)
+    livro_id = resultado_cadastro['data']['id_livro']
+
+    dadosEdicao = {
+        "id_livro": livro_id,
+        "nome_livro": "1984",
+        "sinopse_livro": "Ele curte várias fita, várias balada"
+    }
+
+    resultado = editar_livros(dadosEdicao)
+    assert resultado['status'] == status.HTTP_200_OK
+    assert resultado['data']['nome_livro'] == "1984"
+    assert resultado['data']['sinopse_livro'] == "Ele curte várias fita, várias balada"
--- backend/catalogo/tests/test_usuario_service.py
+++ backend/catalogo/tests/test_usuario_service.py
@@ -4,6 +4,22 @@ from catalogo.services.usuario_service import listar_usuarios, cadastrar_usuario
 from rest_framework import status
 
 @pytest.mark.django_db
+def test_listar_usuarios_sucesso():
+    dados = {
+        "nome_usuario": "Larry",
+        "email_usuario": "larry@email.com",
+        "senha_usuario": "1234"
+    }
+    cadastrar_usuarios(dados)
+    resultado = listar_usuarios().data
+
+    assert isinstance(resultado, list)
+    assert len(resultado) == 1
+    assert resultado[0]['nome_usuario'] == "Larry"
+    assert resultado[0]['email_usuario'] == "larry@email.com"
+    assert resultado[0]['senha_usuario'] == "1234"
+
+@pytest.mark.django_db
 def test_cadastrar_usuarios_sucesso():
     dados = {
         "nome_usuario": "Larry",
@@ -17,3 +33,26 @@ def test_cadastrar_usuarios_sucesso():
     assert resultado['data']['senha_usuario'] == "1234"
     assert Usuario.objects.count() == 1
     
+@pytest.mark.django_db
+def test_editar_usuarios_sucesso():
+    dados = {
+        "nome_usuario": "Larry",
+        "email_usuario": "larry@email.com",
+        "senha_usuario": "1234"
+    }
+
+    resultado_cadastro = cadastrar_usuarios(dados)
+    usuario_id = resultado_cadastro['data']['id_usuario']
+
+    dadosEdicao = {
+        "id_usuario": usuario_id,
+        "nome_usuario": "Larry Barry",
+        "email_usuario": "larryBarry@email.com",
+        "senha_usuario": "1234"
+    }
+
+    resultado = editar_usuario(dadosEdicao)
+    assert resultado['status'] == status.HTTP_200_OK
+    assert resultado['data']['nome_usuario'] == "Larry Barry"
+    assert resultado['data']['email_usuario'] == "larryBarry@email.com"
+    assert resultado['data']['senha_usuario'] == "1234"
